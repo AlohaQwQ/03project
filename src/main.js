87c6d3f0fffc7a650b999a7e53b511bf062be0e9
AlohaQwQ <757993938@qq.com>
2024-11-09 21:14:11 +0800

    remove

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'; // 引入路由配置
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
